c7a921309fe43049ad464b70f94d0effff3df398
yasshikasah362 <114586710+yasshikasah362@users.noreply.github.com>
2023-02-13 23:34:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.to("#main>video",{
+    duration:3,
+    ease:Expo.easeInOut,
+    opacity:1,
+    
+})
+
+gsap.to(".section2>h1",{
+    duration:2,
+    ease:Expo.easeInOut,
+    opacity:1,
+    delay:1,
+    y:100
+})
+gsap.to(".section2>h2",{
+    duration:2,
+    ease:Expo.easeInOut,
+    opacity:1,
+    delay:2,
+    y:100
+})
+gsap.to(".section2>.text",{
+    duration:2,
+    ease:Expo.easeInOut,
+    opacity:1,
+    delay:3,
+    y:100
+})
